6434409ffb041da5b1b2afa5224736dd25d8d675
Aneesh Sharma <ansh993@gmail.com>
2024-03-17 18:57:59 -0400

    fixing keys

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -6,7 +6,7 @@ import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCJ5mnBxcj3VUrSce_5CStPfyfetJ8jUWw",
+    apiKey: `${process.env.REACT_APP_FIREBASE_KEY}`,
     authDomain: "view-vault.firebaseapp.com",
     projectId: "view-vault",
     storageBucket: "view-vault.appspot.com",
