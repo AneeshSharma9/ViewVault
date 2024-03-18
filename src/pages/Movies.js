6d578345cd4867b450fbddfb05b7d0b9df048014
Aneesh Sharma <ansh993@gmail.com>
2024-03-18 18:18:42 -0400

    styling movie page

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -89,7 +89,7 @@ const Movies = () => {
                 return b.watched - a.watched;
             });
             setMovies(sortedMovies);
-        } else{
+        } else {
             fetchMovies(uid);
         }
     };
@@ -142,61 +142,79 @@ const Movies = () => {
     return (
         <div className="">
             <Navbar />
-            <div className="container">
-                <nav className="nav nav-pills nav-fill p-4">
-                    <a className="nav-item nav-link active" href="/movies">Movies</a>
-                    <a className="nav-item nav-link" href="/tvshows">TV Shows</a>
-                    <a className="nav-item nav-link" href="/anime">Anime</a>
-                    <a className="nav-item nav-link" href="/manga">Manga</a>
-                </nav>
+            <div className="container-fluid">
 
-                <h1 className="text-center">Movie Watchlist</h1>
+                <h1 className="text-center m-4">Movie Watchlist</h1>
 
-                <div className="pt-2 pb-4">
-                    <div className="dropdown mb-2">
-                        <button className="btn btn-outline-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                            {sortBy}
-                        </button>
-                        <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton1">
-                            <li><button className="dropdown-item" onClick={() => handleSortBy("To Watch")}>To Watch</button></li>
-                            <li><button className="dropdown-item" onClick={() => handleSortBy("Watched")}>Watched</button></li>
-                            <li><button className="dropdown-item" onClick={() => handleSortBy("Default")}>Default</button></li>
+                <div className="row">
+                    <div className="text-center col-lg-2 col-md-3 col-sm-11 position-fixed border m-5 rounded shadow p-4">
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <h4 className="text-center">Watchlists</h4>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="/movies">Movies</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">TV Shows</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Anime</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Manga</a>
+                            </li>
                         </ul>
                     </div>
-                    <div className="list-group list-group-light">
-                        {movies.map((movie) => (
-                            <li key={movie.id} className="list-group-item rounded mb-2 mt-2 shadow p-3 bg-white d-flex justify-content-between align-items-center">
-                                <div className="form-check">
-                                    <input className="form-check-input" type="checkbox" value={movie.watched} id={`checkboxExample${movie.id}`} checked={movie.watched} onChange={() => handleToggleWatched(movie.id, movie.watched)} />
-                                    <label className="form-check-label ml-2 fw-bold" htmlFor={`checkboxExample${movie.id}`}>{movie.name}</label>
-                                    <p>
-                                        <span class={`badge rounded-pill ${getBackgroundColor(movie.vote_average)}`}>{(movie.vote_average * 10).toFixed(2)}%</span>
-                                        {' '}
-                                        <span class="badge bg-light text-dark border border-danger">{movie.agerating}</span>
-                                        {' '}
-                                        <span className="fst-italic">{convertMinToHrMin(movie.runtime)}</span>
-                                    </p>
-                                    {movie.providers && movie.providers.length > 0 && (
-                                        <p>Stream On: {movie.providers.join(', ')}</p>
-                                    )}
-                                </div>
-                                <div className="d-flex align-items-center justify-content-between">
-                                    <div class="btn-group dropstart m-2">
-                                        <button type="button" class="btn btn-outline-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">≡</button>
-                                        <ul class="dropdown-menu">
-                                            <li><a class="dropdown-item" onClick={() => { toComponentB(movie) }}>More like this</a></li>
-                                            <li><a class="dropdown-item" onClick={() => { toDopebox(movie.name) }}>Stream on DopeBox</a></li>
-                                            <li><a class="dropdown-item" onClick={() => { toLookmovie(movie.name) }}>Stream on Lookmovie</a></li>
-                                        </ul>
-                                    </div>
-                                    <button className="btn btn-outline-danger" onClick={() => handleRemoveMovie(movie.id)}>X</button>
-                                </div>
-                            </li>
-                        ))}
+
+                    <div className="col-lg-8 col-md-7 col-sm-12 offset-lg-3 offset-md-4">
+                        <div className="pt-2 pb-4">
+                            <div className="dropdown mb-2">
+                                <button className="btn btn-outline-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                    {sortBy}
+                                </button>
+                                <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton1">
+                                    <li><button className="dropdown-item" onClick={() => handleSortBy("To Watch")}>To Watch</button></li>
+                                    <li><button className="dropdown-item" onClick={() => handleSortBy("Watched")}>Watched</button></li>
+                                    <li><button className="dropdown-item" onClick={() => handleSortBy("Default")}>Default</button></li>
+                                </ul>
+                            </div>
+                            <div className="list-group list-group-light">
+                                {movies.map((movie) => (
+                                    <li key={movie.id} className="list-group-item rounded mb-2 mt-2 shadow p-3 bg-white d-flex justify-content-between align-items-center">
+                                        <div className="form-check">
+                                            <input className="form-check-input" type="checkbox" value={movie.watched} id={`checkboxExample${movie.id}`} checked={movie.watched} onChange={() => handleToggleWatched(movie.id, movie.watched)} />
+                                            <label className="form-check-label ml-2 fw-bold" htmlFor={`checkboxExample${movie.id}`}>{movie.name}</label>
+                                            <p>
+                                                <span class={`badge rounded-pill ${getBackgroundColor(movie.vote_average)}`}>{(movie.vote_average * 10).toFixed(2)}%</span>
+                                                {' '}
+                                                <span class="badge bg-light text-dark border border-danger">{movie.agerating}</span>
+                                                {' '}
+                                                <span className="fst-italic">{convertMinToHrMin(movie.runtime)}</span>
+                                            </p>
+                                            {movie.providers && movie.providers.length > 0 && (
+                                                <p>Stream On: {movie.providers.join(', ')}</p>
+                                            )}
+                                        </div>
+                                        <div className="d-flex align-items-center justify-content-between">
+                                            <div class="btn-group dropstart m-2">
+                                                <button type="button" class="btn btn-outline-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">≡</button>
+                                                <ul class="dropdown-menu">
+                                                    <li><a class="dropdown-item" onClick={() => { toComponentB(movie) }}>More like this</a></li>
+                                                    <li><a class="dropdown-item" onClick={() => { toDopebox(movie.name) }}>Stream on DopeBox</a></li>
+                                                    <li><a class="dropdown-item" onClick={() => { toLookmovie(movie.name) }}>Stream on Lookmovie</a></li>
+                                                </ul>
+                                            </div>
+                                            <button className="btn btn-outline-danger" onClick={() => handleRemoveMovie(movie.id)}>X</button>
+                                        </div>
+                                    </li>
+                                ))}
+                            </div>
+                        </div>
+                        <div className="text-center mb-5 ">
+                            <a className="btn btn-primary" href="./searchmovie">Add Movie</a>
+                        </div>
                     </div>
-                </div>
-                <div className="text-center">
-                    <a className="btn btn-primary" href="./searchmovie">Add Movie</a>
                 </div>
             </div>
         </div>
